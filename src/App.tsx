c05736b2865b0aa1cfe777fd9ae2084461a1ee51
blobby-bobby <bulle.ouvrard@gmail.com>
2024-01-04 12:12:18 +0100

    first commit: install sass and bases

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+function App() {
+
+  return (
+    <>
+      <nav>
+        <div>Light</div>
+        <div>Logo</div>
+        <div>Info</div>
+      </nav>
+
+      <div>
+        <h1>Awesome Sci-Fi Art</h1>
+        <p>Welcome to a selection of great sci-fi works where Artificial Intelligence have probably digged for those pictures we have all seen on the Internet quite recently. Guarantee made by humans.</p>
+      </div>
+
+      <nav>
+        <div>Share</div>
+        <div>Source code</div>
+      </nav>
+    </>
+  )
+}
+
+export default App
